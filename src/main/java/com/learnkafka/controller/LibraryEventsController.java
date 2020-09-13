e7028d8d5c396ec076a006dc56b7d6b4f9f6e7f1
ravsanth <ravsanth@cisco.com>
2020-09-14 01:20:57 +0530

    Integrated Kafka producer events with controller

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,18 +1,30 @@
 package com.learnkafka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.producer.LibraryEventProducer;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvents(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvents(@RequestBody LibraryEvent libraryEvent)
+	    throws JsonProcessingException {
+	log.info("Library post event..");
+	libraryEventProducer.sendLibraryEvents(libraryEvent);
 	return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
